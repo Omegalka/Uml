3fa3b45bcd5d32202bd3dc8cfb9c538a29906c3e
Omegalka <134694780+Omegalka@users.noreply.github.com>
2024-04-16 20:35:41 +0300

    Add files via upload

---
+++ praktika/praktika/praktika.cpp
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <string>
+using namespace std;
+
+class NameInfo {
+public:
+    string firstname;
+    string surname;
+    string lastname;
+};
+
+class LocationInfo {
+public:
+    string country;
+    string region;
+    string city;
+    string street;
+    int house_number;
+};
+
+class DateOfBirth {
+public:
+    int day;
+    int month;
+    int year;
+};
+
+class EnrollmentInfo {
+public:
+    DateOfBirth start_date;
+    int kurs;
+    string group_name;
+    string specialization;
+};
+
+class AcademicPerformance {
+public:
+    int* dz_rates;
+    int dz_count;
+    int* practice_rates;
+    int practice_count;
+    int* exam_rates;
+    int exam_count;
+    int* zachet_rates;
+    int zachet_count;
+};
+
